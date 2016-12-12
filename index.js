d2bfa4c856c438ecf34a085c5157eff0824bd161
JFschmiechen <jfschmiechen@gmail.com>
2016-12-12 01:41:18 -0800

    added database entry and listeners

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 var mainText = document.getElementById("mainText");
 var returnField = document.getElementById("returnField");
-
-var fieldRef = firebase.database().ref().child("text");
-
-fieldRef.on('value', function(textSnap) {
-   returnField.innerText = textSnap.val();
-});
+var fieldRef = firebase.database().ref();
 
 function sendClick() {
+  var timeStamp = firebase.database.ServerValue.TIMESTAMP;
+  var msgCount = 1;
   firebaseRef = firebase.database().ref();
 
   var messageText = mainText.value;
-  firebaseRef.child("text").set(messageText);
-
+    firebaseRef.push(messageText);
+    var temp = firebaseRef.key;
+    firebaseRef.temp.ref().child('timeID').set(timeStamp); // Error, syntax most likely wrong
 }
+
+fieldRef.on('child_added', function(textSnap, id) {
+
+   id = textSnap.key;
+   var div = document.createElement('p'); // creates new p tag in chatbox
+   chatArea.appendChild(div);
+   div.textContent = textSnap.val();
+});
+
+fieldRef.on('child_removed', function(textSnap, id) {
+
+  id = chatArea.firstChild.key;
+  chatArea.removeChild(chatArea.firstChild);
+})
