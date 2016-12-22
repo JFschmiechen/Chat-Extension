f9bf5a01c164a95c78b273621beb4319efd52c20
JFschmiechen <jfschmiechen@gmail.com>
2016-12-21 19:31:22 -0800

    added manifest for chrome
    - added room service which allows creation and joining of different chat rooms
    - added naming rooms and setting passwords

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+var config = {
+  apiKey: "AIzaSyCAwawwT34ytgXyy5F6KDAKMVErYYlBqYM",
+  authDomain: "chatapp-7fa59.firebaseapp.com",
+  databaseURL: "https://chatapp-7fa59.firebaseio.com",
+  storageBucket: "chatapp-7fa59.appspot.com",
+  messagingSenderId: "805518429060"
+};
+firebase.initializeApp(config);
 const mainText = document.getElementById("mainText");
 const returnField = document.getElementById("returnField");
-const fieldRef = firebase.database().ref('messageLog');
+const sendBtn = document.getElementById('sendBtn');
+const fieldRef = firebase.database().ref(name);
+
 var userEmail;
 var timeStamp = firebase.database.ServerValue.TIMESTAMP;
 var count = 0;
@@ -12,16 +22,20 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 })
 
+sendBtn.onclick = function(){sendClick()};
 function sendClick() {
-  firebaseRef = firebase.database().ref('messageLog');
+  firebaseRef = firebase.database().ref(name);
 
   var messageText = mainText.value;
     firebaseRef.child(totalMsg).set({messageText,
-	timeStamp,
-	count,
-	});
+	    timeStamp,
+      count
+  	});
+    totalMsg++;
+    count++;
   if (totalMsg >= 3) {
     fieldRef.child(totalMsg - count).remove();
+    count--;
   }
 }
 
@@ -30,13 +44,13 @@ fieldRef.on('child_added', function(textSnap, id) {
    var div = document.createElement('p'); // creates new p tag in chatbox
    chatArea.appendChild(div);
    div.textContent = userEmail + ": " + textSnap.val().messageText;
-   count++;
-   totalMsg++;
+
 
 });
 
 fieldRef.on('child_removed', function(textSnap, id) {
   id = chatArea.firstChild.key;
   chatArea.removeChild(chatArea.firstChild);
-  count--;
 });
+
+// Room Service
--- loginAuth.js
+++ loginAuth.js
@@ -1,8 +1,21 @@
+
 const email = document.getElementById('emailField');
 const pass = document.getElementById('passField');
 const btnLogin = document.getElementById('login');
 const btnSignUp = document.getElementById('signUp');
 const btnLogout = document.getElementById('logOut');
+const logOutInner = document.getElementById('logOutInner');
+const invalidText = document.getElementById('invalidEmail');
+const createBtn = document.getElementById('createRm');
+const joinBtn = document.getElementById('joinRm');
+const rmService = document.getElementById('roomService');
+const rmName = document.getElementById('rmName');
+const rmPass = document.getElementById('rmPass');
+const finalizeRmBtn = document.getElementById('finalizeRm');
+const createRmForm = document.getElementById('createForm');
+const backToRmS = document.getElementById('backToRmS');
+const db = firebase.database().ref();
+var name;
 
 // Login
 
@@ -22,8 +35,11 @@ btnSignUp.addEventListener('click', e => {
   const emailVal = email.value;
   const passVal = pass.value;
   const auth = firebase.auth();
-
-  const promise = auth.createUserWithEmailAndPassword(emailVal, passVal);
+  if ((emailVal.includes('@') && emailVal.includes('.com'))) {
+    const promise = auth.createUserWithEmailAndPassword(emailVal, passVal);
+  } else {
+    invalidText.removeAttribute('hidden');
+  }
 
   promise.catch(e => console.log(e.message));
 });
@@ -34,19 +50,50 @@ btnLogout.addEventListener('click', e => {
   firebase.auth().signOut();
 });
 
+logOutInner.addEventListener('click', e => {
+  firebase.auth().signOut();
+})
 // Auth state change
 
 firebase.auth().onAuthStateChanged(firebaseUser => {
   if (firebaseUser) {
     console.log(firebaseUser);
+    rmService.removeAttribute('hidden');
     btnLogout.removeAttribute('hidden');
-    container.removeAttribute('hidden');
     loginForm.setAttribute('hidden', 'loginForm');
-
+    invalidText.setAttribute('hidden', 'invalidText');
   } else {
     console.log("logged out");
+    rmService.setAttribute('hidden', 'rmService');
     btnLogout.setAttribute('hidden', 'btnLogout');
     container.setAttribute('hidden', 'container');
     loginForm.removeAttribute('hidden');
   }
 });
+
+// Back to menu
+
+if (backToRmS) {
+  backToRmS.addEventListener('click', e => {
+    rmService.removeAttribute('hidden');
+    createRmForm.setAttribute('hidden', 'createRmForm');
+  })
+}
+// Create room
+
+if (createBtn) {
+  createBtn.addEventListener('click', e => {
+    createRmForm.removeAttribute('hidden');
+    rmService.setAttribute('hidden', 'rmService');
+  })
+}
+
+if (finalizeRmBtn) {
+  finalizeRmBtn.addEventListener('click', e => {
+    name = rmName.value;
+    var pass = rmPass.value;
+    db.child(name).set(pass);
+    container.removeAttribute('hidden');
+    createRmForm.setAttribute('hidden', 'createRmForm');
+  })
+}
